d484b87449bcaaa0490410fc55894d0f3d2e3832
nayliner <38292577+nayliner@users.noreply.github.com>
2018-04-11 10:40:57 -0600

    update footer

--- footer_info.php
+++ footer_info.php
@@ -1,15 +1,8 @@
 <footer class="footer">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-6 slogan">
-				<nav class="">
-					<ul>
-						<li class="slogan"><a href="javascript:voice(0)" style="padding:10px 15px">WEAVING A BETTER WEB</a></li>
-					</ul>
-				</nav>
-			</div>
 			<div class="col-md-6" style="text-align:right">
-				<p class="copyright"> &copy; <script>document.write(new Date().getFullYear())</script> Powered by <a href="http://ranksol.com" target="_blank">Ranksol</a></p>
+				<p class="copyright"> &copy; <script>document.write(new Date().getFullYear())</script> Powered by <a href="http://nayliner.com" target="_blank">Nayliner</a></p>
 			</div>
 		</div>
 	</div>
